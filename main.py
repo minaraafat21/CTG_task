711b53ba710ca06c9d1aa3d12a1012a0b1b127a7
mina <minaraafatemil14@gmail.com>
2024-12-19 00:05:07 +0200

    plot

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import pandas as pd
+import pyqtgraph as pg
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
@@ -7,6 +9,7 @@ class MyWindow(QMainWindow):
         super().__init__()
         uic.loadUi('EQ.ui', self)
         self.resize(1000, 500)
+
         self.FHR_graph.showGrid(x=True, y=True)
         self.FHR_graph.setLabel('left', 'FHR', units='bpm')
         self.FHR_graph.setLabel('bottom', 'Time', units='s')
@@ -17,8 +20,22 @@ class MyWindow(QMainWindow):
         self.UC_grah.setLabel('bottom', 'Time', units='s')
         self.UC_grah.setTitle('UC Graph')
 
+        # Set the initial text on the labels
         self.FHR_label.setText('FHR: 150 bpm')
         self.CTG_label.setText('CTG Interpretation: Normal')
+
+        self.load_and_plot_data('1001.csv')
+
+    def load_and_plot_data(self, filename):
+        data = pd.read_csv(filename)
+
+        timestamps = data['timestamp'].values
+        fhr = data['fhr'].values
+        uc = data['uc'].values
+
+        self.FHR_graph.plot(timestamps, fhr, pen='r')  # Red line for FHR
+
+        self.UC_grah.plot(timestamps, uc, pen='g')  # Green line for UC
 
 
 if __name__ == '__main__':
